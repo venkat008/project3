ebd3f37a9c1f13f68e9159580762620f81bf9cac
venkat008 <venkat.vn1092@gmail.com>
2015-11-30 14:14:19 -0800

    updated version of project3

--- js/script.js
+++ js/script.js
@@ -1,55 +1,119 @@
-$(function() {
-var albumData,
-albumItems,
-artistName,
-itunesUrl,
-$albumList = $('.album-list');
-$('#album-search').on('submit', function(event) {
-$('.initial').hide();
-event.preventDefault();
-$('#loaader').show();
-$albumList.empty();
-albumData, albumItems = '',
-artistName = $('#artist-name').val().replace(/ /g, '+'),
-itunesUrl = 'https://api.instagram.com/v1/tags/'+artistName+'/media/recent?count=12&client_id=fed3ea7d36644c4993d81a12621c74c8';
-$.ajax({
-method: 'GET',
-url: itunesUrl,
-dataType: 'jsonp'
-})
-.done(function(results) {
-albumData = results.data;
-if ( albumData.length !== 0 ) {
-$.each(albumData, function(key, value) {
-albumItems += '<div class= "pics">';
-albumItems += '<img src="' + value.images.standard_resolution.url + '" />';
-albumItems += '<div class="container">';
-albumItems += '<div class="profile">';
-albumItems += '<img src="' + value.caption.from.profile_picture + '" />';
-albumItems += '</div>';
-albumItems += '<ul class="tags">';
-albumItems +='<li>';
-albumItems += '<p>' + value.caption.from.username + '</p>';
-albumItems += '<p>'
-albumItems += '<i class="fa fa-heart"></i> ' +value.likes.count+ ' ';
-albumItems += '<i class="fa fa-comments"></i> ' +value.comments.count;
-albumItems += '</p>'
-albumItems += '</li>';
-albumItems += '</ul>';
-albumItems += '</div>';
-albumItems += '</div>';
-});
-} else {
-albumItems += '<p style="margin-top: 18px;">Sorry, images not found.</p>';
-}
-$albumList.append(albumItems);
- })
-      .fail(function() {
-         $albumList.append('<li>Sorry! There was a problem, please try again.</li>');
+$(document).ready(function(){
+  $('#more').hide();
+   $('.fancybox').fancybox();
+
+$(function(){
+  var tagData,
+  nextData,
+  tagItems,
+  tagName,
+  instagramUrl,
+  tagItems2,
+  nextUrl,
+  loadB,
+  $tagList = $('.tag-list');
+  $('#tag-name-item').on('click', function(event) {
+  event.preventDefault();
+    // reset all the things
+     $('.initial').hide();
+     $('#loaader').show();
+     $('#more').show();
+     $tagList.empty();
+     tagData, tagItems = '',
+      // get the search string
+       tagName = $('#tag-name').val().replace(/ /g, '+'),
+      instagramUrl= 'https://api.instagram.com/v1/tags/'+tagName+'/media/recent?count=12&client_id=a22c00bd31ca48e8b3dfc67b51b44262';
+      $.ajax({
+        method: 'GET',
+        url: instagramUrl,
+        dataType: 'jsonp'
       })
-      .always(function(){
-      $('#loaader').hide();
+
+    .done(function(data) {
+      nextUrl = data.pagination.next_url;
+      tagData = data.data;
+     if ( tagData.length !== 0 ) {
+     $('#loaader').hide();
+     $.each(tagData, function(key, value) {
+        tagItems += '<div class= "pics">';
+        tagItems += '<a class="fancybox" rel="gallery" href="'+ value.images.standard_resolution.url +'" class="fancybox" rel="gallery">';
+        tagItems += '<img src="' + value.images.standard_resolution.url + '" />';
+        tagItems += '</a>';
+        tagItems += '<div class="container">';
+        tagItems += '<div class="profile">';
+        tagItems += '<img src="' + value.caption.from.profile_picture + '" />';
+        tagItems += '</div>';
+        tagItems += '<ul class="tags">';
+        tagItems +='<li>';
+        tagItems += '<p>' + value.caption.from.username + '</p>';
+        tagItems += '<p>'
+        tagItems += '<i class="fa fa-heart"></i> ' +value.likes.count+ ' ';
+        tagItems += '<i class="fa fa-comments"></i> ' +value.comments.count;
+        tagItems += '</p>'
+        tagItems += '</li>';
+        tagItems += '</ul>';
+        tagItems += '</div>';
+        tagItems += '</div>';
     });
 
+     $('#more').on('click',function(event){
+     event.preventDefault();
+     tagItems2 = '',
+
+       $.ajax({
+        method: 'GET',
+        url: nextUrl,
+        next_max_id: "13872296",
+        dataType: 'jsonp'
+      })
+
+     .done(function(dat){
+     tagData = dat.data;
+     nextUrl = dat.pagination.next_url;
+     $('#lmore').toggle();
+   if (tagData.length!==0){
+    $.each(tagData,function(key,value){
+        tagItems2 += '<div class= "pics">';
+        tagItems2 += '<a class="fancybox" rel="gallery" href="'+ value.images.standard_resolution.url +'" class="fancybox" rel="gallery">';
+        tagItems2 += '<img src="' + value.images.standard_resolution.url + '" />';
+        tagItems2 += '</a>';
+        tagItems2 += '<div class="container">';
+        tagItems2 += '<div class="profile">';
+        tagItems2 += '<img src="' + value.caption.from.profile_picture + '" />';
+        tagItems2 += '</div>';
+        tagItems2 += '<ul class="tags">';
+        tagItems2 +='<li>';
+        tagItems2 += '<p>' + value.caption.from.username + '</p>';
+        tagItems2 += '<p>'
+        tagItems2 += '<i class="fa fa-heart"></i> ' +value.likes.count+ ' ';
+        tagItems2 += '<i class="fa fa-comments"></i> ' +value.comments.count;
+        tagItems2 += '</p>'
+        tagItems2 += '</li>';
+        tagItems2 += '</ul>';
+        tagItems2 += '</div>';
+        tagItems2 += '</div>';
+    });
+           }
+               $tagList.append(tagItems2);
+               $('#lmore').show();
+             })
+
+            })
+          }
+
+
+       else {
+         $('#loaader').hide();
+            tagItems += '<p style="margin-top: 18px;">Sorry, artist not found.</p>';
+         }
+         $tagList.append(tagItems);
+      })
+      // and if it fails...
+      .fail(function() {
+        $('#loaader').hide();
+         $tagList.append('<li>Sorry! There was a problem, please try again.</li>');
+
+      });
    });
+});
 });
